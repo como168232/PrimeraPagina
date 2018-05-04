910777163dd82bbde6fe2af03ad050ad32a99c26
como168232 <como168232@upemor.edu.mx>
2018-05-04 09:18:34 -0500

    Arreglos

--- php/primero.php
+++ php/primero.php
@@ -1,3 +1,12 @@
 <?php
-	echo "Mi primer codigo PHP";
+	//Array asociativo
+	$familia = array('Juan' => 'Sinuhe' ,'Altulo' => 'Azucena' );
+	echo 'Juan papa de '.$familia['Juan'].' y Altulo marido de '.$familia['Altulo'].'<br>';
+	//Array multidimensional
+	$usuarios= array('Mario',array(23, array('Hombre','Mujer')));
+	echo $usuarios[1][1][0].'<br>';
+	echo $usuarios[1][1][1].'<br>';
+	echo $usuarios[1][0].'<br>';
+	echo $usuarios[0].'<br>';
+
 ?>
